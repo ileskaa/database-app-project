2c5b24667c497bd51e0c7b60a4ceacd8aa61c5c1
ileska <akseli.ingervo@gmail.com>
2024-09-09 09:26:07 +0300

    Create members table

---
+++ sql/members.sql
@@ -0,0 +1,4 @@
+CREATE TABLE members (id SERIAL PRIMARY KEY, fname VARCHAR(35), lname VARCHAR(35));
+
+INSERT INTO members (fname, lname) VALUES ('John', 'Doe');
+INSERT INTO members (fname, lname) VALUES ('Aku', 'Ankka');
