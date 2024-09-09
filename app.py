22a7fd625b3ff171e066a7c136b34dcd6df31328
ileska <akseli.ingervo@gmail.com>
2024-09-09 09:38:38 +0300

    Add form enabling users to sign up

--- app.py
+++ app.py
@@ -1,8 +1,39 @@
 from flask import Flask
+from flask import render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 from os import getenv
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+@app.route("/createuser", methods=["POST"])
+def createuser():
+    username = request.form["username"]
+    password = request.form["password"]
+    hash_value = generate_password_hash(password)
+    sql = text(
+        "INSERT INTO users"
+        "(username, password) VALUES (:username, :password)"
+    )
+    db.session.execute(sql, {"username": username, "password": hash_value})
+    db.session.commit()
+
+    fname = request.form["fname"]
+    lname = request.form["lname"]
+    sql = text("INSERT INTO members (fname, lname) VALUES (:fname, :lname)")
+    db.session.execute(sql, {"fname": fname, "lname": lname})
+    db.session.commit()
+
+    session["username"] = username
+
+    return redirect("/")
+
