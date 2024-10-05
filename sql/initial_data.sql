7d54167d1d04fb05bb8d8252353ab704b4c422af
ileskaa <akseli.ingervo@gmail.com>
2024-10-05 19:10:27 +0300

    Add some initial data to the classes table

---
+++ sql/initial_data.sql
@@ -0,0 +1,14 @@
+INSERT INTO classes VALUES (
+    'Kuviokellunta',
+    'Kuviokellunta on kaikenikäisillä sopiva harrastus, jossa kellutaan ihanasti veden varassa muodostaen kauniita rymäkuvioita. Tässä lajissa eivät taatustikaan nivelet kärsi.'
+);
+
+INSERT INTO classes VALUES (
+    'Brasilialainen Zouk',
+    'Brasilialainen Zouk on paritanssi joka kehittyi 1990-luvulla. Lambada nimisestä Brasilialaisesta tanssista. Zoukin nimi tulee Zouk-musiikista, jonka tahtiin lajia alunperin tanssittiin. Nykyään Zouk-tanssin musiikkirepertuaari on kuitenkin huomattavasti laajentunut, ja sitä tanssitaan muun muassa myös R''n''B, hip hop ja pop -musiikkiin.'
+);
+
+INSERT INTO classes VALUES (
+    'Kriketti',
+    'Kriketti on Englannissa alkunsa saanut mailapallopeli. Siinä kaksi 11-henkistä joukkuetta ottaa mittaa toisistaanja tavoitteena on tehdä enemmän juoksuja kuin vastustajajoukkue. Kriketin arvioidaan olevan maailman toiseksi suosituin laji, jalkapallon jälkeen.'
+);
