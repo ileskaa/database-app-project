59f3525e86e3d99a350d27caaf7198662a5883b6
ileskaa <akseli.ingervo@gmail.com>
2024-10-05 19:10:12 +0300

    Add a classes table to the database schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,2 +1,12 @@
-CREATE TABLE members (id SERIAL PRIMARY KEY, fname VARCHAR(35), lname VARCHAR(35));
-CREATE TABLE users (id SERIAL PRIMARY KEY, username VARCHAR(35), password TEXT, admin BOOLEAN);
+CREATE TABLE members (
+    id SERIAL PRIMARY KEY, fname VARCHAR(35), lname VARCHAR(35)
+);
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY, username VARCHAR(35), password TEXT, admin BOOLEAN
+);
+
+CREATE TABLE classes (
+    name VARCHAR(50) UNIQUE,
+    description TEXT
+);
