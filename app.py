3a688deaeb87f97ad9ceed4d90acd0ac0589e054
ileskaa <akseli.ingervo@gmail.com>
2024-10-05 19:11:29 +0300

    Move authentication functionality to its own module

--- app.py
+++ app.py
@@ -5,3 +5,4 @@ app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
 
 import routes # noqa: F401 E402 E261
+import auth # noqa: F401 E402 E261
--- routes.py
+++ routes.py
@@ -1,14 +1,16 @@
 from db import db
 from app import app
-from flask import render_template, request, redirect, session
+from flask import render_template, request, redirect
 from sqlalchemy.sql import text
-from sqlalchemy import exc
-from werkzeug.security import check_password_hash, generate_password_hash
+import urllib.parse
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    sql = text("SELECT name FROM classes")
+    result = db.session.execute(sql)
+    classes = result.fetchall()
+    return render_template("index.html", classes=classes)
 
 
 @app.route("/send", methods=["POST"])
@@ -22,65 +24,15 @@ def send():
     return redirect("/")
 
 
-def createuser():
-    username = request.form["username"]
-    password = request.form["password"]
-    hash_value = generate_password_hash(password)
-    sql = text(
-        "INSERT INTO users"
-        "(username, password) VALUES (:username, :password)"
-    )
-    try:
-        db.session.execute(sql, {"username": username, "password": hash_value})
-    except exc.DataError:
-        error = "The username should not exceed 35 characters"
-        return render_template("signup.html", error=error)
-
-    fname = request.form["fname"]
-    lname = request.form["lname"]
-    sql = text("INSERT INTO members (fname, lname) VALUES (:fname, :lname)")
-    try:
-        db.session.execute(sql, {"fname": fname, "lname": lname})
-    except exc.DataError:
-        error = "The username should not exceed 35 characters"
-        return render_template("signup.html", error=error)
-
-    db.session.commit()
-
-    session["username"] = username
-
-    return redirect("/")
+# You can put variable names in your view functions
+@app.route("/class/<name>")
+def classes(name):
+    return render_template("class.html", name=name)
 
 
-@app.route("/signup", methods=["GET", "POST"])
-def signup():
-    if request.method == "POST":
-        return createuser()
-    else:
-        return render_template("signup.html")
-
-
-@app.route("/login", methods=["POST"])
-def login():
-    username = request.form["username"]
-    password = request.form["password"]
-    sql = text("SELECT id, password FROM users WHERE username=:username")
-    result = db.session.execute(sql, {"username": username})
-    user = result.fetchone()
-    if not user:
-        print("invalid user")
-        return render_template("index.html", error="Invalid credentials")
-    else:
-        hash_value = user.password
-        if check_password_hash(hash_value, password):
-            session["username"] = username
-            return redirect("/")
-        else:
-            print("invalid pswd")
-            return render_template("index.html", error="Invalid credentials")
-
-
-@app.route("/logout")
-def logout():
-    del session["username"]
-    return redirect("/")
+@app.context_processor
+def utility_processor():
+    def encode_url(string):
+        safe_string = urllib.parse.quote_plus(string)
+        return safe_string
+    return dict(encode_url=encode_url)
