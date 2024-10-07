7971c71681d675977762b97246a98c846449f83a
ileskaa <akseli.ingervo@gmail.com>
2024-10-07 13:35:56 +0300

    Add constraints to the database schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,14 +1,23 @@
 CREATE TABLE members (
+    -- Primary key means unique and not null
     id SERIAL PRIMARY KEY, fname VARCHAR(35), lname VARCHAR(35)
 );
 
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY, username VARCHAR(35), password TEXT, admin BOOLEAN
+    username VARCHAR(35) PRIMARY KEY,
+    password TEXT NOT NULL,
+    admin BOOLEAN NOT NULL
 );
 
 CREATE TABLE classes (
+    name VARCHAR(50) PRIMARY KEY,
+    description TEXT NOT NULL
+);
+
 CREATE TABLE enrollments (
     class VARCHAR(50) REFERENCES classes (name),
     username VARCHAR(35) REFERENCES users (username),
     PRIMARY KEY (class, username)
 );
+
+-- Might need a comments table?
