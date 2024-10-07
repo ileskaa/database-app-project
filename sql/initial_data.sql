5ce53271f34b3a151bc6dfc36f4bad564309927b
ileskaa <akseli.ingervo@gmail.com>
2024-10-07 13:41:25 +0300

    Fix typos in class descriptions

--- sql/initial_data.sql
+++ sql/initial_data.sql
@@ -5,10 +5,10 @@ INSERT INTO classes VALUES (
 
 INSERT INTO classes VALUES (
     'Brasilialainen Zouk',
-    'Brasilialainen Zouk on paritanssi joka kehittyi 1990-luvulla. Lambada nimisestä Brasilialaisesta tanssista. Zoukin nimi tulee Zouk-musiikista, jonka tahtiin lajia alunperin tanssittiin. Nykyään Zouk-tanssin musiikkirepertuaari on kuitenkin huomattavasti laajentunut, ja sitä tanssitaan muun muassa myös R''n''B, hip hop ja pop -musiikkiin.'
+    'Brasilialainen Zouk on paritanssi joka kehittyi 1990-luvulla Lambada nimisestä Brasilialaisesta tanssista. Zoukin nimi tulee Zouk-musiikista, jonka tahtiin lajia alunperin tanssittiin. Nykyään Zouk-tanssin musiikkirepertuaari on kuitenkin huomattavasti laajentunut, ja sitä tanssitaan muun muassa myös R''n''B, hip hop ja pop -musiikkiin.'
 );
 
 INSERT INTO classes VALUES (
     'Kriketti',
-    'Kriketti on Englannissa alkunsa saanut mailapallopeli. Siinä kaksi 11-henkistä joukkuetta ottaa mittaa toisistaanja tavoitteena on tehdä enemmän juoksuja kuin vastustajajoukkue. Kriketin arvioidaan olevan maailman toiseksi suosituin laji, jalkapallon jälkeen.'
+    'Kriketti on Englannissa alkunsa saanut mailapallopeli. Siinä kaksi 11-henkistä joukkuetta ottaa mittaa toisistaan ja tavoitteena on tehdä enemmän juoksuja kuin vastustajajoukkue. Kriketin arvioidaan olevan maailman toiseksi suosituin laji, jalkapallon jälkeen.'
 );
