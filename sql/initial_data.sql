de3292099d3fb0a51a8ba081b506eeced9ed70b8
ileskaa <akseli.ingervo@gmail.com>
2024-10-12 07:49:41 +0300

    Add an admin user

--- sql/initial_data.sql
+++ sql/initial_data.sql
@@ -12,3 +12,9 @@ INSERT INTO classes VALUES (
     'Kriketti',
     'Kriketti on Englannissa alkunsa saanut mailapallopeli. Siinä kaksi 11-henkistä joukkuetta ottaa mittaa toisistaan ja tavoitteena on tehdä enemmän juoksuja kuin vastustajajoukkue. Kriketin arvioidaan olevan maailman toiseksi suosituin laji, jalkapallon jälkeen.'
 );
+
+INSERT INTO users VALUES (
+    'admin',
+    'scrypt:32768:8:1$86mF34EX3sbXBncp$636277adce3450fe7aaebd122feaac76db04d4558d52ebf47c501158f275533903e9652871fec69a0d91796d6c37f8e246ac7c635f9c41818fbfc79289fd87aa',
+    TRUE
+);
