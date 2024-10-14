36bb42816d4e0495fa3c48786a7e60cd3941a5e2
ileskaa <akseli.ingervo@gmail.com>
2024-10-14 16:35:54 +0300

    Update the database schema and the initial data

--- sql/initial_data.sql
+++ sql/initial_data.sql
@@ -12,30 +12,51 @@ INSERT INTO classes VALUES
     'Kriketti on Englannissa alkunsa saanut mailapallopeli. Siinä kaksi 11-henkistä joukkuetta ottaa mittaa toisistaan ja tavoitteena on tehdä enemmän juoksuja kuin vastustajajoukkue. Kriketin arvioidaan olevan maailman toiseksi suosituin laji, jalkapallon jälkeen.'
 );
 
+INSERT INTO members(fname, lname, email) VALUES
+(
+    'Aku',
+    'Ankka',
+    'aku.ankka@kwak.fi'
+),
+(
+    'James',
+    'Bobond',
+    '008@mi.six'
+),
+(
+    'Iida',
+    'Ihmetyyppi',
+    'i.iida@tehoposti.fi'
+);
+
 INSERT INTO users VALUES
 (
     'admin',
     -- keepsecret
     'scrypt:32768:8:1$86mF34EX3sbXBncp$636277adce3450fe7aaebd122feaac76db04d4558d52ebf47c501158f275533903e9652871fec69a0d91796d6c37f8e246ac7c635f9c41818fbfc79289fd87aa',
-    TRUE
+    TRUE,
+    NULL
 ),
 (
     'aku964',
     -- oispajoperjantai
     'scrypt:32768:8:1$DjyRPGGPhbhGmavr$7341ddce37b3b48d7e406d6078971bef95c7b69645d7732db2b96486c8a9f071cf99e629aed6aa2aeb140fec33567340450bd967f7ed69aa5b38a636dfb9a771',
-    FALSE
+    FALSE,
+    1
 ),
 (
     'smartboyXZ',
     -- foobar
     'scrypt:32768:8:1$1UnADmG2IjoIN0Y5$48a5ec54738af7df0a11431151098f257c2103777cc5b3f6ec8be6d72b8a27be1de6a6993846f0f860408c6c737495f4454c2ec94c86c0daaa73fbf74eb8040b',
-    FALSE
+    FALSE,
+    2
 ),
 (
     'IidaLOL',
     -- oleniidahehe
     'scrypt:32768:8:1$043MOIodhm6Hnh6m$1839deb4ba8555cbafa9b87f1c9e3c0832169a22e242b7cb891a88db7fb8d4c5d94839546bcde94d7cb4898cb73623fcf2a288af73990dfe925061c1de1416ab',
-    FALSE
+    FALSE,
+    3
 );
 
 INSERT INTO comments(classname, username, comment) VALUES
--- sql/schema.sql
+++ sql/schema.sql
@@ -1,12 +1,16 @@
 CREATE TABLE members (
     -- Primary key means unique and not null
-    id SERIAL PRIMARY KEY, fname VARCHAR(35), lname VARCHAR(35)
+    id SERIAL PRIMARY KEY,
+    fname VARCHAR(35),
+    lname VARCHAR(35),
+    email VARCHAR(254)
 );
 
 CREATE TABLE users (
     username VARCHAR(35) PRIMARY KEY,
     password TEXT NOT NULL,
-    admin BOOLEAN NOT NULL DEFAULT FALSE
+    admin BOOLEAN NOT NULL DEFAULT FALSE,
+    member_id INTEGER REFERENCES members(id) ON DELETE CASCADE
 );
 
 CREATE TABLE classes (
@@ -16,13 +20,13 @@ CREATE TABLE classes (
 
 CREATE TABLE comments (
     id BIGINT GENERATED ALWAYS AS IDENTITY,
-    classname VARCHAR(50) REFERENCES classes (name),
-    username VARCHAR(35) REFERENCES users (username),
+    classname VARCHAR(50) REFERENCES classes (name) ON DELETE CASCADE,
+    username VARCHAR(35) REFERENCES users (username) ON DELETE CASCADE,
     comment TEXT NOT NULL
 );
 
 CREATE TABLE enrollments (
-    class VARCHAR(50) REFERENCES classes (name),
-    username VARCHAR(35) REFERENCES users (username),
+    class VARCHAR(50) REFERENCES classes (name) ON DELETE CASCADE,
+    username VARCHAR(35) REFERENCES users (username) ON DELETE CASCADE,
     PRIMARY KEY (class, username)
 );
