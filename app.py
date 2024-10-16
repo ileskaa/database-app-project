a1be1ead5a8b8a41af1ca30e6bf41afcf39fd3c0
ileskaa <akseli.ingervo@gmail.com>
2024-10-16 09:56:48 +0300

    Add secure and samesite attributes to the session cookie

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from os import getenv
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
+app.config.update(
+    SESSION_COOKIE_SECURE=True,
+    SESSION_COOKIE_SAMESITE='Strict',
+)
 
 import routes # noqa: F401 E402 E261
 import auth # noqa: F401 E402 E261
